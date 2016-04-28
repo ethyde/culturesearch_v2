a4fee26e58dc31297a220c3e114b133599fb10c6
Emmanuel Plouvier <eplouvier@shivacom.fr>
2016-04-28 17:40:13 +0200

    wip

--- app/scripts/modules/FormComponent.js
+++ app/scripts/modules/FormComponent.js
@@ -2,90 +2,33 @@
 
 import React from 'react';
 
-class FormButton extends React.Component {
-
+class Form extends React.Component {
+    
     constructor(props) {
         super(props);
-        this.content = { data: [] };
-        this.loadData = this.loadData.bind( this );
+        // this.state = {likesCount : 0};
+        this.state = { data: [] };
+        this.onLike = this.onLike.bind(this);
     }
 
-    loadData() {
-
-        fetch(this.props.url)
+    onLike () {
+        // let newLikesCount = this.state.data;
+        // this.setState({data: newLikesCount});
+        
+        fetch('http://www.omdbapi.com/?t=frozen&y=&plot=short&r=json')
             .then(response => response.json())
             .then(data => this.setState({ data: data }))
             .catch(err => console.error(this.props.url, err.toString()));
-
-        console.log(this.content);
-
-    }
-
-    render() {
-        return (
-            <button onClick={this.loadData}>ok</button>
-        );
-    }
-}
-
-class FormInput extends React.Component {
-
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {likesCount : 0};
-    //     this.onLike = this.onLike.bind(this);
-    // }
-
-    // onLike () {
-    //     let newLikesCount = this.state.likesCount + 1;
-    //     this.setState({likesCount: newLikesCount});
-    // }
-
-    // render() {
-    //     return (
-    //         <div>
-    //             Likes : <span>{this.state.likesCount}</span>
-    //             <div>
-    //                 <button onClick={this.onLike}>Like Me</button>
-    //             </div>
-    //         </div>
-    //     );
-    // }
-
-    constructor(props) {
-        super(props);
-        this.state = {inputValue: ''};
-    }
-
-    handleChange( e ) {
-        this.setState({ inputValue: e.target.value });
-    }
-
-    render() {
-        return (
-            <input
-                type='text'
-                value={this.state.inputValue}
-                ref="inputVal"
-                onChange={this.handleChange.bind( this )} />
-        );
-    }
-
-}
-
-class Form extends React.Component {
-
-    constructor(props) {
-        super(props);
     }
 
     render() {
         return (
             <div>
-                <FormInput />
-
-                <FormButton 
-                    url={`http://www.omdbapi.com/?t=${this.refs.inputVal}&y=&plot=short&r=json`} />
+                Likes : <span>{this.state.data}</span>
+                <div>
+                    <button 
+                        onClick={this.onLike}>Like Me</button>
+                </div>
             </div>
         );
     }
